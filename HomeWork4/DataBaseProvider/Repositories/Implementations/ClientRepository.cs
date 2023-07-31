b07996d1f499587381f6f8b8932d213e55d941a6
chkqq <124921297+chkqq@users.noreply.github.com>
2023-07-31 16:22:57 +0300

    fix

--- HomeWork4/DataBaseProvider/Repositories/Implementations/ClientRepository.cs
+++ HomeWork4/DataBaseProvider/Repositories/Implementations/ClientRepository.cs
@@ -6,10 +6,12 @@ namespace DatabaseProvider.Repositories.Implementations
     public class ClientRepository : Repository<Client>, IClientRepository
     {
         public ClientRepository(ApplicationContext context) : base(context) { }
+        
         public List<Client> GetAll()
         {
             return Entities.ToList();
         }
+        
         public Client? GetById(int id)
         {
             return Entities.FirstOrDefault(p => p.ClientId == id);
