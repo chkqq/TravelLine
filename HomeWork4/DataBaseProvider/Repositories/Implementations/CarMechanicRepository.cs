c4ec8c8a5b4b668ae13d091f09c4000dfc3d07bc
chkqq <124921297+chkqq@users.noreply.github.com>
2023-07-31 16:22:17 +0300

    добавил перенос строки

--- HomeWork4/DataBaseProvider/Repositories/Implementations/CarMechanicRepository.cs
+++ HomeWork4/DataBaseProvider/Repositories/Implementations/CarMechanicRepository.cs
@@ -7,18 +7,22 @@ namespace DatabaseProvider.Repositories.Implementations
     public class CarMechanicRepository : Repository<CarMechanic>, ICarMechanicRepository
     {
         public CarMechanicRepository(ApplicationContext context) : base(context) { }
+        
         public List<CarMechanic> GetAll()
         {
             return Entities.ToList();
         }
+        
         public CarMechanic? GetById(int id)
         {
             return Entities.FirstOrDefault(r => r.CarMechanicId == id);
         }
+        
         public List<CarMechanic> GetByClientId(int id)
         {
             return Entities.Where(r => r.ClientId == id).ToList();
         }
+        
         public List<CarMechanic> GetByCarId(int id)
         {
             return Entities.Where(r => r.CarId == id).ToList();
