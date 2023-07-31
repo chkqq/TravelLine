116b85753668eac7566835f91e4e9f9b6954d091
chkqq <124921297+chkqq@users.noreply.github.com>
2023-07-31 16:22:39 +0300

    fix

--- HomeWork4/DataBaseProvider/Repositories/Implementations/CarRepository.cs
+++ HomeWork4/DataBaseProvider/Repositories/Implementations/CarRepository.cs
@@ -7,10 +7,12 @@ namespace DatabaseProvider.Repositories.Implementations
     public class CarRepository : Repository<Car>, ICarRepository
     {
         public CarRepository(ApplicationContext context) : base(context) { }
+        
         public List<Car> GetAll()
         {
             return Entities.ToList();
         }
+        
         public Car? GetById(int id)
         {
             return Entities.FirstOrDefault(d => d.CarId == id);
