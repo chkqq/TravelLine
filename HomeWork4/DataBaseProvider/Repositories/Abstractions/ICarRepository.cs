96ec5d1382aeb35df6d447ce72ba76cb78ba6900
chkqq <124921297+chkqq@users.noreply.github.com>
2023-08-01 09:53:30 +0300

    fix

--- HomeWork4/DataBaseProvider/Repositories/Abstractions/ICarRepository.cs
+++ HomeWork4/DataBaseProvider/Repositories/Abstractions/ICarRepository.cs
@@ -6,6 +6,6 @@ namespace DatabaseProvider.Repositories.Abstractions
     public interface ICarRepository : IRepository<Car>
     {
         public List<Car> GetAll();
-        public Car? GetById(int id);
+        public Car GetById(int id);
     }
 }
